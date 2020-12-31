fd4dc7d91d56ee21a294e247a905cde6bde58cd8
semo4 <fotboy1788@hotmail.com>
2020-12-31 16:35:18 +0200

    remove consolelog

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 
 var name = prompt('What is your name?');
-console.log("Hello Mr/Mrs " + name);
+
 alert("Hello Mr/Mrs " + name);
 alert("let's play aguessing game about me? \n Please answer with yes/no or y/n");
 
@@ -59,18 +59,18 @@ switch(qeustionFive.toLowerCase())
 {
     case 'y':
     case 'yes':
-        console.log("thank you for your time  ");
+      
         alert("thank you for your time  ");
         arrayOfAnswers.push(["qeustionFive ", " Correct Answer"]);
         totalScore++;
         break;
     case 'no':
     case 'n':
-        console.log("thank you for your time  ");
+        
         alert("thank you for your time  ");
         break;
     default:
-        console.log("Please make sure to input yes/no");
+       
         alert("Please make sure to input yes/no");
         arrayOfAnswers.push(["qeustionFive ", " Wrong Answer"]);
         break;
@@ -90,44 +90,44 @@ while(i < 4)
     var checkAnswer = "";
     var questionSix = parseInt(prompt("enter guess number between 1 and 50 ? \n you have "+ counter+" opportunity to guess the correct number"));
     if(questionSix >=  1  && questionSix < rand){
-        console.log('Too Low');
+      
         checkAnswer = checkAnswer + " Too Low \n";
-        //alert('Too Low');
+      
     }
     else if (questionSix <= 50 && questionSix > rand){
-        console.log('Too High');
+       
         checkAnswer = checkAnswer + " Too High \n";
-        //alert('Too High');
+        
     }
     else if (isNaN(questionSix)){
-        console.log('Please make sure to enter a number');
+       
         checkAnswer = checkAnswer + " Please make sure to enter a number \n";
-       // alert('Please make sure to enter a number ');
+       
     }
     else if(questionSix === rand){
-        console.log('you have the correct answer');
+       
         alert('you have the correct answer');
         totalScore++;
         break;
     }
     counter--;
     if(counter !== 0){
-        console.log(checkAnswer +'you stll have  '+ counter + '  opportunity try again');
+        
         alert(checkAnswer +'you stll have  '+ counter + '  opportunity try again');
     }else{
-        console.log('you take all opportunity');
+       
         alert('you take all opportunity');
     }
 
     if(i === 3){
-        console.log('Oh I am sorry you did not guess  \n The correct answer is '+ rand);
+       
         alert('Oh I am sorry you did not guess  \n The correct answer is '+ rand);
     }
     i++;
 }
 
 
-var correctAnswer = ['golf mk3 gti','bmw e45','shalpy 500gt','eleanor 1969','hellcat','g-class']
+var correctAnswer = ['golf','bmw','ford','eleanor 1969','hellcat','g-class']
 var count = 6;
 var j = 0;
 while(j < 6)
@@ -135,44 +135,42 @@ while(j < 6)
     var questionSaven =prompt(" guess what is my favorit car ? \n you have "+ count +" opportunity to guess the correct number");
     var checkCarAnswer = "" ;
     if(questionSaven === null){
-        console.log('you have did not input any thing ');
-        //alert('you have did not input any thing ');
+       
         checkCarAnswer = checkCarAnswer + 'you have did not input any thing \n';
     }
     else{
         if(correctAnswer.indexOf(questionSaven.toLowerCase()) !== -1){
-            console.log('you have guess the correct answer');
+            
             alert('you have guess the correct answer \n');
             totalScore++;
             var cars = 'this is all correct answer \n';
             for(var x = 0; x < correctAnswer.length; x++){
-                 console.log('Answer number ' + (x+1) + ' is : '+ correctAnswer[x].toUpperCase())
+                
                  cars = cars +' Answer number ' + (x+1) + ' is : '+ correctAnswer[x].toUpperCase() +'\n';
              }
-            console.log(cars);
+          
             alert(cars);
             break;
         }
         else{
-            console.log('you did not guess the correct answer');
-            //alert('you did not guess the correct answer');
+            
             checkCarAnswer = checkCarAnswer + 'you did not guess the correct answer \n';
         }
          count--;
          if(count !== 0){
-           console.log(checkCarAnswer + ' you stll have  '+ count + '  opportunity try again');
+           
           alert(checkCarAnswer + ' you stll have  '+ count + '  opportunity try again');
          }else{
-            console.log('you take all opportunity');
+          
             alert('you take all opportunity');
          }
      
          if(j === 5){
-             console.log('Oh I am sorry you did not guess The correct answer');
+           
              alert('Oh I am sorry you did not guess The correct answer ');
              var car = 'this is all correct answer \n';
              for(var k = 0; k < correctAnswer.length; k++){
-                 console.log('Answer number ' + (k+1) + ' is : '+ correctAnswer[k].toUpperCase())
+                
                  car = car +' Answer number ' + (k+1) + ' is : '+ correctAnswer[k].toUpperCase() +'\n';
              }
              console.log(car);
@@ -189,20 +187,20 @@ alert("thank you for your time  Mr/Mrs " + name + "\n Your Score is : " + totalS
 
 
 function alertWrongAswer(message, questionNumber){
-    console.log("No It's wrong answer, " +message );
+   
     alert("No It's wrong answer, " +message);
     arrayOfAnswers.push([questionNumber, " Wrong Answer"]);
 }
 
 function alertCorrectAswer(message, questionNumber){
-    console.log("Yes It's right answer, " +message );
+    
     alert("Yes It's right answer, " +message);
     arrayOfAnswers.push([questionNumber, " Correct Answer"]);
     totalScore++;
 }
 
 function alertWrongInput(){
-    console.log("Please make sure to input yes/no or y/n");
+    
     alert("Please make sure to input yes/no or y/n");
 }
 
@@ -211,6 +209,6 @@ function answerScore(){
     for (var c = 0; c< arrayOfAnswers.length ; c++){
         answer = answer + arrayOfAnswers[c] +"\n";
     }
-    console.log(answer);
+   
     alert( answer);
 }
